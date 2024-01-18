bfa6e1f9efcb5e1406c28f7efeac17ab55afb341
Erik Cupsa <2003eec@gmail.com>
2024-01-18 15:07:09 -0500

    Fixed bug

---
+++ src/components/Home/index.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import { Link } from "react-router-dom";
+import LogoMcGill from "../../assets/McGillLogo.png"
+import "./index.scss";
+
+const Home = () => {
+    return(
+        <>
+            <div className = "container home-page">
+                <div className = "text-zone">
+                    <h1>
+                        <img src={LogoMcGill} alt = "McGill Exam Scheduler"/>
+                    </h1>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Home
