94da4a5057fa904de007039eb21bbd1be7f6e75a
Erik Cupsa <2003eec@gmail.com>
2024-01-18 15:24:58 -0500

    Started work on nav bar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React, { useEffect } from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-// import Teams from './components/Teams';
-// import TeamData from './components/TeamData';
-// import Nation from "./components/Nation";
-// import Position from "./components/Position";
-// import Search from "./components/Search";
+import './App.scss';
+import Layout from "./components/Layout";
+import React from 'react';
+import Home from "./components/Home";
 
 function App() {
-  useEffect(() => {
-    document.title = 'PremierZone Fantasy';
-  }, []);
-
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          {/* <Route path="teams" element={<Teams />} />
-          <Route path="data" element={<TeamData />} />
-          <Route path="nation" element={<Nation />} />
-          <Route path="position" element={<Position />} />
-          <Route path="search" element={<Search />} /> */}
         </Route>
       </Routes>
     </>
